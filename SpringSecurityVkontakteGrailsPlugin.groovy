77d20fae5ab16411a92609ebeb1d5530694448bc
Burt Beckwith <burt@burtbeckwith.com>
2014-02-25 09:43:10 -0500

    dropped minimum Grails version to 2.0

--- SpringSecurityVkontakteGrailsPlugin.groovy
+++ SpringSecurityVkontakteGrailsPlugin.groovy
@@ -31,7 +31,7 @@ class SpringSecurityVkontakteGrailsPlugin {
     // the plugin version
     def version = "1.0-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.2 > *"
+    def grailsVersion = "2.0 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
